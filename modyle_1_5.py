e53a61ba0b78bc86143394c6413fe2ed6dcc97af
Sergey446128 <s.milyaev2018@yandex.ru>
2024-08-27 14:39:41 +0300

    Add files via upload

---
+++ modyle_1_5.py
@@ -0,0 +1,11 @@
+# immutable_var = 1, 2, 3, 'NAME', 'String'
+# print(immutable_var)
+# immutable_var = 1, 2, 3, 'Name', 'String'
+# print(immutable_var)
+# immutable_var [1] = 100
+# print(immutable_var)
+
+mutable_list = ([1, 2, 3], 4 )
+print(mutable_list)
+mutable_list [0][1] = 5
+print(mutable_list)

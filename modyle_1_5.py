331e291ab64834838724226e8514e10de666f699
Sergey446128 <s.milyaev2018@yandex.ru>
2024-08-28 11:07:31 +0300

    Add files via upload

--- modyle_1_5.py
+++ modyle_1_5.py
@@ -1,7 +1,8 @@
 immutable_var = ([1, 2, 3], 'Name', 'String')
 print(immutable_var)
-immutable_var[0][1] = 4
-print(immutable_var)
+# immutable_var[0] = 100  # Кортеж относится к не изменяемым типа данных,кортеж не поддерживает обращение
+                          # по элементам, по этому не получится изменить элементы кортежа.
+# print(immutable_var)
 
 # mutable_list = ([1, 2, 3], 4 )
 # print(mutable_list)
